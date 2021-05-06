d6601c15fa2944b462cfee507920d1bb5a596028
Anton <enitefall81@gmail.com>
2021-05-05 20:06:52 -0700

    Add Meal entries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react"
 import FoodForm from "./FoodForm"
 import FoodList from "./FoodList"
 
+let initialMealEntries = [
+  {mealName: "Breakfast", timestamp: true},
+  {mealName: "Lunch", timestamp: true},
+  {mealName: "Dinner", timestamp: false},
+]
+
 function App() {
   return <div>
 <FoodForm/>
